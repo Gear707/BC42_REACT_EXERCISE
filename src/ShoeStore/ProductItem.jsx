f0c619c7c4d851b52eb1230c58c10ec24812848b
Nghia <nghia3977@gmail.com>
2023-03-18 12:40:19 +0700

    fix the icon

--- src/ShoeStore/ProductItem.jsx
+++ src/ShoeStore/ProductItem.jsx
@@ -8,7 +8,7 @@ function ProductItem({ product }) {
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text" style={{ fontSize: 20 }}>${product.price}</p>
                 <a href="#" className="btn btn-success">
-                    View details<i class="bi bi-view-list ms-2"></i>
+                    View details<i className="bi bi-view-list ms-2"></i>
                 </a>
             </div>
         </div>
